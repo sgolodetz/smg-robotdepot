4dcee288a97de8cb79a34dfbbfa783479bd2e990
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-04-05 15:23:10 +0100

    Add some comments.

--- smg/robotdepot/robomaster_client.py
+++ smg/robotdepot/robomaster_client.py
@@ -7,24 +7,34 @@ from typing import Tuple
 
 
 class RobomasterClient:
-    """TODO"""
+    """A client that can be used to communicate with a server running on a DJI Robomaster S1."""
 
     # CONSTRUCTOR
 
-    def __init__(self, endpoint: Tuple[str, int] = ("192.168.2.1", 7860), *, timeout: int = 10):
+    def __init__(self, endpoint: Tuple[str, int] = ("192.168.2.1", 7860), *, timeout: int = 10,
+                 print_commands: bool = True):
+        """
+        Construct a Robomaster client.
+
+        :param endpoint:        The server host and port, e.g. ("192.168.2.1", 7860).
+        :param timeout:         The socket timeout to use (in seconds).
+        :param print_commands:  Whether or not to print commands that are sent.
+        """
         self.__alive: bool = False
+        self.__print_commands: bool = print_commands
         self.__should_terminate: threading.Event = threading.Event()
 
         self.__chassis_fwd: float = 0
         self.__gimbal_yaw: float = 0
 
-        # Connect to the server.
+        # Try to connect to the server.
         try:
             self.__sock: socket.SocketType = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.__sock.connect(endpoint)
             self.__sock.settimeout(timeout)
             self.__alive = True
         except ConnectionRefusedError:
+            # If we can't connect to the server, raise an exception.
             raise RuntimeError("Error: Could not connect to the server")
 
         # Set up the locks.
@@ -53,9 +63,25 @@ class RobomasterClient:
     # PUBLIC METHODS
 
     def set_chassis_fwd(self, rate: float) -> None:
+        """
+        Tell the robot's chassis to move forward at the specified rate.
+
+        .. note::
+            This can also be used to move backwards (by specifying a negative rate).
+
+        :param rate:    The rate at which the robot's chassis should move forward (in [-1,1]).
+        """
         self.__chassis_fwd = RobomasterClient.__rate_to_control_value(rate)
 
     def set_gimbal_yaw(self, rate: float) -> None:
+        """
+        Tell the robot's gimbal to turn at the specified rate.
+
+        .. note::
+            If the robot's chassis is set to follow its gimbal, this will cause the chassis to turn as well.
+
+        :param rate:    The rate at which the gimbal should turn (in [-1,1]).
+        """
         self.__gimbal_yaw = RobomasterClient.__rate_to_control_value(rate)
 
     def terminate(self) -> None:
@@ -82,10 +108,19 @@ class RobomasterClient:
             self.__send_command(cmd)
 
     def __send_command(self, cmd: str) -> None:
-        print(cmd)
+        """
+        Send the specified command to the robot.
+
+        :param cmd: The command to send.
+        """
         with self.__cmd_lock:
+            # Send the command, prepended by its length.
             msg: bytes = cmd.encode("utf-8")
             self.__sock.send(struct.pack("<i", len(msg)) + msg)
+
+            # Print the command that was sent, if desired.
+            if self.__print_commands:
+                print(f"Sent Command: {cmd}")
 
     # PRIVATE STATIC METHODS
 
