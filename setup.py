a54b8d0f0af5e43121ea8e879bff4a0444adcfe7
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-04-05 16:02:45 +0100

    Add a setup.py file.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="smg-robotdepot",
+    version="0.0.1",
+    author="Stuart Golodetz",
+    author_email="stuart.golodetz@cs.ox.ac.uk",
+    description="Interface for controlling a DJI Robomaster S1",
+    long_description="",  #long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sgolodetz/smg-robotdepot",
+    packages=find_packages(include=["smg.robotdepot", "smg.robotdepot.*"]),
+    include_package_data=True,
+    install_requires=[
+        "smg-rotory"  # FIXME: This should ultimately be replaced with an smg-joysticks dependency.
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
